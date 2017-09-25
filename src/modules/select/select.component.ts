aebf5dea47bedf869c9e9784fc27a2764cc99142
penrique <penrique@soflex.com.ar>
2017-09-25 17:55:27 -0300

    input/output mapping

--- src/modules/select/select.component.ts
+++ src/modules/select/select.component.ts
@@ -13,10 +13,10 @@ import { Subscription } from "rxjs/Subscription";
     `],
     template: `
     <div class="" [ngStyle]="{color:formControl.errors?'#f44336':'inherit'}" style="margin-top: 10px">
-        <md-select [formControl]="formControl" [disabled]="formControl.disabled" [style.width]="to.nonull?'100%':'calc(100% - 50px)'" style="padding-top: 4px" [placeholder]="to.placeholder" [multiple]="to.multiple">
+        <md-select [(ngModel)]="value" (ngModelChange)="changed($event)" [disabled]="formControl.disabled" [style.width]="to.nullable?'calc(100% - 50px)':'100%'" style="padding-top: 4px" [placeholder]="to.placeholder" [multiple]="to.multiple">
             <md-option *ngFor="let item of items" [value]="item">{{item.name}}</md-option>
         </md-select>
-        <button md-icon-button *ngIf="!to.nonull" (click)="clear()"><i class="material-icons md-24">clear</i></button>
+        <button md-icon-button *ngIf="to.nullable" (click)="clear()"><i class="material-icons md-24">clear</i></button>
     </div>
     `
 })
@@ -24,6 +24,7 @@ export class FormlySelectComponent extends Field implements OnInit, OnDestroy {
 
     private ngUnsubscribe: Subject<void> = new Subject<void>();
     public items: any;
+    public value: any;
 
     constructor() {
         super();
@@ -31,85 +32,63 @@ export class FormlySelectComponent extends Field implements OnInit, OnDestroy {
 
     ngOnInit() {
         this.to.disabled && this.formControl.disable();
-        if (this.to.source) {
-            this.to.source.takeUntil(this.ngUnsubscribe).subscribe(x => {
-                this.items = x;
-                if (x && x.length > 0) {
-                    if (!this.to.multiple) {
-                        if (x && this.formControl.value) {
-                            let filtered = x.filter(y => y.value == this.formControl.value.value);
-                            if (filtered && filtered.length > 0) {
-                                this.formControl.setValue(filtered[0]);
-                            }
-                        }
-                        if (!this.formControl.value && this.to.nonull && x && x.length > 0) {
-                            this.formControl.setValue(x[0]);
-                        }
-                    }
-                    else {
-                        if (this.formControl.value) {
-                            let filtered = [];
-                            for (var i = 0, len1 = this.items.length; i < len1; i++) {
-                                let a = this.items[i];
-                                for (var j = 0, len2 = this.formControl.value.length; j < len2; j++) {
-                                    let b = this.formControl.value[j];
-                                    if (a.value == b.value) {
-                                        filtered.push(a);
-                                        break;
-                                    }
-                                }
-                            }
-                            if (filtered && filtered.length > 0) {
-                                this.formControl.setValue(filtered);
-                            }
-                        }
+        this.to.initialized && this.to.initialized(this.formControl.value);
+        this.to.source && this.to.source.takeUntil(this.ngUnsubscribe).subscribe(x => {
+            this.items = x;
+            if (x && x.length > 0) {
+                if (this.formControl.value || this.formControl.value === 0) {
+                    let filtered = x.filter(y => { 
+                        return this.inputMapFn(y) == this.inputMapFn(this.formControl.value) 
+                    });
+                    if (filtered && filtered.length > 0) {
+                        this.outputMapFn(filtered[0]);
+                        this.value = filtered[0];
                     }
                 }
-            });
-        }
-        let lastVal = this.formControl.value;
-        this.formControl.valueChanges.takeUntil(this.ngUnsubscribe).filter(x=> x != lastVal).subscribe(x => {
-            lastVal = x;
-            if (this.items && this.items.length > 0) {
-                if (!this.to.multiple) {
-                    if (this.items && x) {
-                        let filtered = this.items.filter(y => y.value == x.value);
-                        if (filtered && filtered.length > 0) {
-                            lastVal = filtered[0];
-                            this.formControl.setValue(filtered[0]);
-                        }
-                    }
-                    if (!x && this.to.nonull && this.items && this.items.length > 0) {
-                        lastVal = this.items[0];
-                        this.formControl.setValue(this.items[0]);
-                    }
-                }
-                else {
-                    if (x) {
-                        let filtered = [];
-                        for (var i = 0, len1 = this.items.length; i < len1; i++) {
-                            let a = this.items[i];
-                            for (var j = 0, len2 = x.length; j < len2; j++) {
-                                let b = x[j];
-                                if (a.value == b.value) {
-                                    filtered.push(a);
-                                    break;
-                                }
-                            }
-                        }
-                        if (filtered && filtered.length > 0) {
-                            lastVal = filtered;
-                            this.formControl.setValue(filtered);
-                        }
-                    }
+                else if (!this.to.nullable) {
+                    this.outputMapFn(this.items[0]);
+                    this.value = this.items[0];
                 }
             }
-            this.to.changed && this.to.changed(x);
+        });
+        let lastVal = this.inputMapFn(this.formControl.value);
+        this.formControl.valueChanges.takeUntil(this.ngUnsubscribe).subscribe(x => {
+            if (lastVal != x) {
+                lastVal = x;
+                if (this.items && this.items.length > 0 && x) {
+                    let filtered = this.items.filter(y => this.inputMapFn(y) == this.inputMapFn(x));
+                    if (filtered && filtered.length > 0) {
+                        lastVal = this.inputMapFn(filtered[0]);
+                        this.outputMapFn(filtered[0]);
+                        this.value = filtered[0];
+                    }
+                }
+                this.to.changed && this.to.changed(x);
+            }
         });
     }
 
+    changed(e) {
+        this.outputMapFn(e);
+    }
+
+    inputMapFn(e: any) {
+        if (this.to.mapFn) {
+            return this.to.mapFn(e);
+        }
+        return e;
+    }
+
+    outputMapFn(e: any) {
+        if (this.to.mapFn && e) {
+            this.formControl.setValue(this.to.mapFn(e));
+            return;
+        }
+        this.formControl.setValue(e);
+    }
+
     clear() {
-        if (this.to.nonull) {
+        if (!this.to.nullable) {
             return;
         }
         this.formControl.setValue(null);
